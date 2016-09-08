608f030f69cfad5c87a5bda80e3d5a470d9a839d
David Mulligan <davidmulligan@btopenworld.com>
2016-09-08 13:00:21 +0100

    Added travis.yml

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # docker-spring-cloud-config-server
+
+## Overview
+
+A docker image for the [Spring Cloud Config Server](http://cloud.spring.io/spring-cloud-static/spring-cloud.html#_spring_cloud_config_server). Spring Cloud Config provides server and client-side support for externalized configuration in a distributed system.
+
+This docker image is based on the lightweight Alpine Linux openjdk image `openjdk:8-jre-alpine`.
+
+## Usage
+
+To run the docker container:
+```sh
+docker run -p 8888:8080 kcomlabs/docker-spring-cloud-config-server
+```
+
+To run the docker container using docker-compose:
+```sh
+spring-cloud-config-server:
+  image: kcomlabs/docker-spring-cloud-config-server
+  ports:
+    - "8888:8080"
+```
+
+After running this command Spring Cloud Config Server should be listening on port 8888 on your docker host.
+
+You can pass options using the JAVA_OPTS environment variable.
--- src/test/java/com/kcom/SpringCloudConfigServerApplicationTests.java
+++ src/test/java/com/kcom/SpringCloudConfigServerApplicationTests.java
@@ -6,14 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.server.ConfigServerApplication;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(SpringCloudConfigServerApplication.class)
+@SpringApplicationConfiguration(ConfigServerApplication.class)
 @WebIntegrationTest(randomPort = true)
 public class SpringCloudConfigServerApplicationTests {
 
@@ -24,7 +24,7 @@ public class SpringCloudConfigServerApplicationTests {
 
     @Test
     public void loadAProperty() {
-        Environment environment = this.restTemplate.getForObject(format("http://localhost:%d/foo/default", localServerPort), Environment.class);
+        Environment environment = restTemplate.getForObject(String.format("http://localhost:%d/foo/default", localServerPort), Environment.class);
 
         assertEquals("bar-value", environment.getPropertySources().iterator().next().getSource().get("bar"));
     }
