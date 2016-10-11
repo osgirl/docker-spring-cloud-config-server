c559099163fec559790e77bfafa527f8d507bcbd
David Mulligan <davidmulligan@btopenworld.com>
2016-10-11 11:49:29 +0100

    Execluded context stack auto configuration

--- src/main/java/com/kcom/config/server/SpringCloudConfigServerApplication.java
+++ src/main/java/com/kcom/config/server/SpringCloudConfigServerApplication.java
@@ -3,9 +3,10 @@ package com.kcom.config.server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude=ContextStackAutoConfiguration.class)
 @EnableConfigServer
 @Slf4j
 public class SpringCloudConfigServerApplication {
